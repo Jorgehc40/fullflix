9a9e08cf5a7f7fdfc561f71b74ad24716a7ab2d9
JORGE HENRIQUE CUSTÓDIO <jorgehcustodio@gmail.com>
2023-09-29 19:47:51 -0300

    7

--- coverflix/src/App.js
+++ coverflix/src/App.js
@@ -1,12 +1,21 @@
 import Detail from "./pages/detail/Detail";
 import Home from "./pages/home/home";
 import Router from "./router/Router";
+import categories from "./api/api";
 
 
 function App() {
   return (
-    <Home/>
-  );
+    <div className="App">
+      {categories.map((category) => {
+        return<Row 
+                key={category.name}
+                title={category.title}
+                path={category.path}
+              />;
+      })}
+    </div>
+    );
 }
 
 export default App;
--- coverflix/src/api/api.js
+++ coverflix/src/api/api.js
@@ -1,12 +1,45 @@
+const API_KEY = ˜4bfb8622b99eb5234220bf7ecd17190a˜;
 
+const categories = [
+    {
+        name: "trending",
+        title: "Em alta"
+        path: `/trending/all/week?api_key=${API_KEY}&language=pt-BR`
+    },
+    {
+        name: "coverflixOriginals",
+        title: "Originais Coverflix"
+        path: `/discover/tv?api_key=${API_KEY}&language=pt-BR&with_networks=213`
+    },
+    {
+        name: "topRated",
+        title: "Populares"
+        path: `/movie/top_rated?api_key=${API_KEY}&language=pt-BR',
+    },
+    {
+        name: "comedy",
+        title: "Comédias"
+        path: `/discover/tv?api_key=${API_KEY}&language=pt-BR&page=1&with_genres=comedy`
+    },
+    {
+        name: "trending",
+        title: "Em alta"
+        path: `/trending/all/week?api_key=${API_KEY}&language=pt-BR',
+    },
+    {
+        name: "trending",
+        title: "Em alta"
+        path: `/trending/all/week?api_key=${API_KEY}&language=pt-BR`
+    },
+];
 
-import axios from 'axios';
-
-export const getList = (setState) =>{
-    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=4bfb8622b99eb5234220bf7ecd17190a&language=pt-br&page=1`)
-    .then((response)=>{
-        setState(response.data.results);
-    }).catch((error)=>{
-        console.log(error);
-    })
-}
+export const getMovies = async (path) => {
+    try {
+        let url = `https://api.themoviedb.org/3/${path}`;
+        const response = await fetch(url);
+        return await response.json();
+    }   catch (error) {
+        console.log("error getMovies: ", error);
+    }
+};
+export default categories;
